5c8be0e30d3df76a83a2756aee5901e32db32df7
Monstorix <monstorix@outlook.com>
2026-04-19 21:40:19 +0800

    Remove UDFPS support flag from lineage_socrates.mk
    Removed the UDFPS support flag from the build configuration.

--- lineage_socrates.mk
+++ lineage_socrates.mk
@@ -30,8 +30,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Maintainer Name
 INFINITY_MAINTAINER := "MokalLera"
 
-# Whether the device supports Fingerprint On Display
-TARGET_HAS_UDFPS := true
-
 # Whether Including Google Apps
 WITH_GAPPS := true
