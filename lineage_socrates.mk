4c0ea97b4181a055dea5cc3182aa5991696c0924
Monstorix <monstorix@outlook.com>
2026-05-10 17:14:09 +0800

    AviumUI bringup

--- lineage_socrates.mk
+++ lineage_socrates.mk
@@ -22,3 +22,22 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=Redmi/socrates/socrates:16/BP2A.250605.031.A3/OS3.0.303.0.WMKCNXM:user/release-keys
+
+# AviumUI Configs
+AVIUM_MAINTAINER := Monstorix
+AVIUM_SETTINGS_SOC_MODEL_NAME := Qualcomm Snapdragon 8 Gen 2
+AVIUM_SETTINGS_DEVICE_CODENAME := Redmi K60 Pro
+AVIUM_IS_OFFICIAL := false
+WITH_GMS := false
+AVIUM_FORCE_SET_FAKE_PROP := true
+
+# Sign the build
+-include vendor/lineage-priv/keys/keys.mk
+
+# Force using release-keys
+ifeq ($(TARGET_BUILD_VARIANT),user)
+    BUILD_KEYS := release-keys
+endif
+
+# replace the prop for release-keys
+PRODUCT_PROPERTY_OVERRIDES += ro.build.tags=release-keys
