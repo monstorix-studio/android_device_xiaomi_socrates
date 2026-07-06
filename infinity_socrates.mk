813f0d4339432c54ef8161d0578f7b375823e575
Monstorix <monstorix@outlook.com>
2026-07-06 16:34:31 +0800

    bye avium, hello infinity

--- infinity_socrates.mk
+++ infinity_socrates.mk
@@ -12,9 +12,9 @@ $(call inherit-product, build/make/target/product/core_64_bit_only.mk)
 $(call inherit-product, device/xiaomi/socrates/device.mk)
 
 # Inherit LineageOS configurations
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_socrates
+PRODUCT_NAME := infinity_socrates
 PRODUCT_DEVICE := socrates
 PRODUCT_MODEL := 22127RK46C
 PRODUCT_BRAND := Redmi
@@ -23,13 +23,10 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=Redmi/socrates/socrates:16/BP2A.250605.031.A3/OS3.0.303.0.WMKCNXM:user/release-keys
 
-# AviumUI Configs
-AVIUM_MAINTAINER := Monstorix
-AVIUM_SETTINGS_SOC_MODEL_NAME := Qualcomm Snapdragon 8 Gen 2
-AVIUM_SETTINGS_DEVICE_CODENAME := Redmi K60 Pro
-AVIUM_IS_OFFICIAL := false
-WITH_GMS := false
-AVIUM_FORCE_SET_FAKE_PROP := true
+# Infinity Configs
+INFINITY_MAINTAINER := Monstorix
+INFINITY_BUILD_TYPE := UNOFFICIAL
+WITH_GAPPS := false
 
 # Sign the build
 -include vendor/lineage-priv/keys/keys.mk
@@ -41,6 +38,3 @@ endif
 
 # replace the prop for release-keys
 PRODUCT_PROPERTY_OVERRIDES += ro.build.tags=release-keys
-
-# MicroG
-$(call inherit-product-if-exists, vendor/partner_gms/products/gms.mk)
